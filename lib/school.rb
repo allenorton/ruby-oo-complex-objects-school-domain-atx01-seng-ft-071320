0e4dcede479b8beb0da4055b22272e9cc7ebfe02
Allene Norton <norton.allene@gmail.com>
2020-07-14 23:43:20 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,12 @@
 # code here!
+class School
+  attr_accessor :roster
+  def initialize(name)
+    @name = name 
+    @roster = {}
+  end 
+  def add_student(names, grade)
+    @roster[grade] = []
+    @roster[grade] << names 
+  end 
+end 
